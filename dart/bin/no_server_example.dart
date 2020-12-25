6d6500cbb019ed481749079d8123c0aa2a315d7c
Tim Whiting <tim@whitings.org>
2020-12-25 16:19:05 -0600

    update to fix example

--- dart/bin/no_server_example.dart
+++ dart/bin/no_server_example.dart
@@ -4,5 +4,5 @@ import 'package:game_scaffold_dart/src/games/reversi.dart';
 void main(List<String> arguments) {
   Game.registerGeneralEvents();
   ReversiGame.register();
-  registerNoServerClients();
+  registerOnDeviceClients();
 }
